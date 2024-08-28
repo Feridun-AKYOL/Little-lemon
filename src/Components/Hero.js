ec2fff19c39f5ff6ce179f88b5886cd0b1438031
Feridun <feridunakyol34@gmail.com>
2024-08-28 12:07:47 +0200

    activate reserve a Table button

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import reservation_image from "../photos/restauranfood.jpg" ;
-
+import { Link } from 'react-router-dom';
 const Reservation = () => {
     return (
         <section className="reservation">
@@ -8,7 +8,7 @@ const Reservation = () => {
                 <h2>Little Lemon</h2>
                 <p>Chicago</p>
                 <p>We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-                <a href="#" className="btn">Reserve a Table</a>
+                <Link to='booking' className="btn">Reserve a Table</Link>
             </div>
             <div className="reservation-image">
                 <img src={reservation_image} alt="Little Lemon Specials" />
